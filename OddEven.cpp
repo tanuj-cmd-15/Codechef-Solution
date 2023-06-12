f8fc7d20f6a6c378d2d6b2fdecf73b324ac92f39
TUSHAR PAWAR <88785532+tanuj-cmd-15@users.noreply.github.com>
2023-06-12 23:36:55 +0530

    Add files via upload

---
+++ OddEven.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+
+//write a program numbers from 1 to 10 in such way that when numbers is odd add 1 and number is even subtract 1
+//indirect recursive
+//
+//void odd();
+//void even();
+//int n =1;
+//
+//void odd(){
+//	if(n<=10){
+//		printf("%d",n+1);
+//		n++;
+//		even();
+//	}
+//	return;
+//}
+//void even(){
+//	if(n<=10){
+//		printf("%d",n-1);
+//		n++;
+//		odd();
+//	}
+//	return;
+//}
+//
+//int main(){
+//	odd();
+//}
+
+
+
+
+
+
+
+
+//tail recursive
+//
+//void fun(int n){
+//	if(n==0)return;
+//	else printf("%d",n);
+//	return fun(n-1);
+//}
+//
+//int main(){
+//	fun(3);
+//	return 0;
+//}
+
+
+//non - tail recursive
+
